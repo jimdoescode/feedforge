594987ca86c8ae54ad0ef1fdddf811798fef7eb3
Jim <jimdoescode@gmail.com>
2011-06-26 02:42:40 -0400

    Removed some logs that should have been ignored

--- feedforge/logs/log-2011-06-24.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2011-06-24 02:04:48 --> 404 Page Not Found --> admin/config
--- feedforge/logs/log-2011-06-26.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2011-06-26 02:01:22 --> Severity: Notice  --> Undefined property: Admin::$_has_login /Users/jim/Programs/htdocs/feedforge/feedforge/controllers/admin.php 88
-ERROR - 2011-06-26 02:09:47 --> Severity: Notice  --> Use of undefined constant CRYPT_SHA512 - assumed 'CRYPT_SHA512' /Users/jim/Programs/htdocs/feedforge/feedforge/controllers/admin.php 98
-ERROR - 2011-06-26 02:09:47 --> Severity: Notice  --> Use of undefined constant CRYPT_SHA256 - assumed 'CRYPT_SHA256' /Users/jim/Programs/htdocs/feedforge/feedforge/controllers/admin.php 99
-ERROR - 2011-06-26 02:09:47 --> Severity: Notice  --> Undefined property: Admin::$session /Users/jim/Programs/htdocs/feedforge/feedforge/controllers/admin.php 72
-ERROR - 2011-06-26 02:38:20 --> Severity: Notice  --> unserialize() [<a href='function.unserialize'>function.unserialize</a>]: Error at offset 0 of 223 bytes /Users/jim/Programs/htdocs/feedforge/system/libraries/Session.php 723
